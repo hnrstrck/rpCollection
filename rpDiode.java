a67bc137df89ba61d725352d3a4d9aea9e101aa2
hnrstrck <heiner@38.de>
2016-09-27 14:50:58 +0000

    Attribute Reihenfolge geaendert

--- rpDiode.java
+++ rpDiode.java
@@ -4,7 +4,9 @@ public final class rpDiode {
 
     private GpioPinDigitalOutput pin;
     private GpioController gpio;
+    
     private boolean boolInitialisierungErfolgt;
+    private int intPin;
 
     private static final Pin[] pinArray = new Pin[] {RaspiPin.GPIO_00, 
             RaspiPin.GPIO_01, RaspiPin.GPIO_02, RaspiPin.GPIO_03, RaspiPin.GPIO_04, 
@@ -12,8 +14,6 @@ public final class rpDiode {
             RaspiPin.GPIO_09, RaspiPin.GPIO_10, RaspiPin.GPIO_11, RaspiPin.GPIO_12, 
             RaspiPin.GPIO_13, RaspiPin.GPIO_14, RaspiPin.GPIO_15, RaspiPin.GPIO_16, 
             RaspiPin.GPIO_17, RaspiPin.GPIO_18, RaspiPin.GPIO_19, RaspiPin.GPIO_20};
-
-    private int intPin;
 
     rpDiode() {
         gpio = GpioFactory.getInstance();
