0b387e366e509c2e580755f901238100e0187734
hnrstrck <heiner@38.de>
2016-11-22 16:41:40 +0000

    Startskript für Groovy-Shell (mit Kompilieren)

---
+++ GSstartC.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Kompiliere die .java-Dateien passend fuer die Groovy-Shell und 
+# starte die Groovy-Shell (groovysh) und bindet die Pfade fuer Pi4J ein
+
+echo "******************"
+echo "*  rpCollection  *"
+echo "******************"
+echo " "
+
+echo "Kompiliere *.java-Dateien"
+echo " "
+
+javac -classpath '.:classes:/opt/pi4j/lib/*' classes/*.java
+# fuer aeltere Java-Versionen 
+# javac -classpath '.:classes:/opt/pi4j/lib/*' -target 1.7 classes/*.java
+
+
+echo "Starte Groovy-Shell."
+echo "Dies kann einen Moment dauern..."
+echo " "
+
+cd classes && sudo groovysh -classpath '.classes:/opt/pi4j/lib/*'
+
