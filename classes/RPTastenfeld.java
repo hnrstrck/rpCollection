1d91e46a7e5a54ccab6aaa58455aa870acc671d2
Johannes Pieper <pieper@dlrg.de>
2023-03-30 22:44:53 +0200

    Tastenfeld auf pi4j v2 umgebaut

--- classes/RPTastenfeld.java
+++ classes/RPTastenfeld.java
@@ -1,41 +1,44 @@
-import com.pi4j.io.gpio.*;
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalInputConfigBuilder;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalOutputConfigBuilder;
+import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.io.gpio.digital.PullResistance;
 import java.util.ArrayList;
 
 /**
  * Klasse fuer den Anschluss eines 4x4 Tastenfeldes an den Raspberry Pi. Das Tastenfeld kann gefragt werden, welche Taste gerade gedrueckt ist.
- * 
- * @author Luca Michael Mathaea
- * @version 0.991 
+ *
+ * @author Luca Michael Mathaea, Johannes Pieper
+ * @version 2.0
  */
 public final class RPTastenfeld {
     protected char[][] feldMatrix = {{'D', 'C', 'B', 'A'},
                                      {'#', '9', '6', '3'},
                                      {'0', '8', '5', '2'},
                                      {'*', '7', '4', '1'}};
-        
-    private ArrayList<GpioPinDigitalInput> inputPins;
-    private ArrayList<GpioPinDigitalOutput> outputPins;
-    private GpioController gpio;
+
+    private ArrayList<DigitalInput> inputPins = null;
+    private ArrayList<DigitalOutput> outputPins = null;
 
     private boolean boolInitialisierungErfolgt;
-    
+
     /**
     * Erstellt ein neues Objekt der Klasse RPTastenfled mit den Pins direkt als Eingabe. Vorsicht: Pins 2 und 3 sind als inputPins keine validen Pins. Benuztung dieser wird zu einem Error fuehren.
     * @param outputPins Die ersten vier Pins des Tastenfeldes.
     * @param inputPins Die letzten vier Pins des Tastenfeldes.
     */
     public RPTastenfeld(ArrayList<Integer> outputPins, ArrayList<Integer> inputPins) {
-        this.gpio = GpioFactory.getInstance();
         this.boolInitialisierungErfolgt = false;
 
         this.setPins(outputPins, inputPins);
     }
-    
+
     /**
      * Erstellt ein neues Objeckt der Klasse RPTastenfeld ohne die Pins zu setzten. Die Pins muessen manuell gesetzt werden mit der Methode RPTastenfeld.setPins()
      */
     public RPTastenfeld() {
-        this.gpio = GpioFactory.getInstance();
         this.boolInitialisierungErfolgt = false;
     }
 
@@ -49,45 +52,46 @@ public final class RPTastenfeld {
         this.setInputPin(inputPins);
         this.setOutputPin(outputPins);
         this.boolInitialisierungErfolgt = true;
-    } 
+    }
 
     private void setInputPin(ArrayList<Integer> pPins) {
-        this.inputPins = new ArrayList<GpioPinDigitalInput>();
-        System.out.println("Input-Pin gesetzt fuer das Tastenfeld:");
-    
-        try {
+        if (this.inputPins == null) {
+            this.inputPins = new ArrayList<DigitalInput>();
+            Context pi4j = RPEnvironment.getContext();
+            DigitalInputConfigBuilder inputConfig = RPEnvironment.getInputConfig();
             for(int i = 0; i < pPins.size(); i++) {
-                GpioPinDigitalInput pin = gpio.provisionDigitalInputPin(Helfer.pinArray[pPins.get(i)]); 
-                
                 //setzte Pin Resistenz auf Pull Down damit der Default-State des Pins LOW ist und nicht HIGH
-                pin.setPullResistance(PinPullResistance.PULL_DOWN);
-            
-                pin.setShutdownOptions(true, PinState.LOW);
-            
+                DigitalInput pin = pi4j.create(inputConfig
+                    .address(pPins.get(i))
+                    .id("pin" + pPins.get(i))
+                    .pull(PullResistance.PULL_DOWN)
+                );
                 this.inputPins.add(pin);
                 System.out.println("Input-Pin " + pPins.get(i) + " gesetzt");
             }
-        } catch (NullPointerException f) {
-            System.out.println("Error: Pin nicht definiert? Klasse: Tastenfeld (NullPointerException)");
-        } 
-
+            System.out.println("Input-Pins gesetzt fuer das Tastenfeld:");
+        } else {
+            System.out.println("Input-Pin bereits gesetzt:");
+        }
     }
-    
+
     private void setOutputPin(ArrayList<Integer> pPins) {
-        this.outputPins = new ArrayList<GpioPinDigitalOutput>();
-        System.out.println("Output-Pin gesetzt fuer das Tastenfeld:");
-    
-        try {
+          if (this.outputPins == null) {
+            this.outputPins = new ArrayList<DigitalOutput>();
+            Context pi4j = RPEnvironment.getContext();
+            DigitalOutputConfigBuilder outputConfig = RPEnvironment.getOutputConfig();
             for(int i = 0; i < pPins.size(); i++) {
-                GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(Helfer.pinArray[pPins.get(i)]);
-                
-                pin.setShutdownOptions(true, PinState.LOW);
-            
+                DigitalOutput pin = pi4j.create(outputConfig
+                    .address(pPins.get(i))
+                    .id("pin" + pPins.get(i))
+                    .shutdown(DigitalState.LOW)
+                );
                 this.outputPins.add(pin);
                 System.out.println("Output-Pin " + pPins.get(i) + " gesetzt");
-            }   
-        } catch (NullPointerException f) {
-            System.out.println("Error: Pin nicht definiert? Klasse: Tastenfeld (NullPointerException)");
+            }
+            System.out.println("Output-Pins gesetzt fuer das Tastenfeld:");
+        } else {
+            System.out.println("Output-Pins bereits gesetzt:");
         }
     }
 
@@ -99,18 +103,16 @@ public final class RPTastenfeld {
     public char gibGedrueckteTaste() {
         if (boolInitialisierungErfolgt == true) {
             try {
-                char letzterGefundenerChar = ' '; 
+                char letzterGefundenerChar = ' ';
                 for(int t = 0; t < this.outputPins.size(); t++) {
-                    this.outputPins.get(t).setState(PinState.HIGH);
-        
+                    this.outputPins.get(t).on();
+
                     for(int i = 0; i < this.inputPins.size(); i++) {
-                        PinState state = this.inputPins.get(i).getState();
-            
-                        if(state == PinState.HIGH) {
+                        if (this.inputPins.get(i).state() == DigitalState.HIGH) {
                             letzterGefundenerChar = this.feldMatrix[t][i];
                         }
                     }
-                    this.outputPins.get(t).setState(PinState.LOW);
+                    this.outputPins.get(t).off();
                 }
                 if(letzterGefundenerChar != ' ') {
                     return letzterGefundenerChar;
@@ -122,26 +124,7 @@ public final class RPTastenfeld {
         } else {
             System.out.println("Zuerst Pins fuer das Membranfeld angeben, bevor diese Methode (gibGedrueckteTaste) wieder aufgerufen wird.");
         }
-        
+
         return ' ';
-    }
-    
-    /**
-    * Schalte GPIO ab und dereferenziere den GPIO und den Pin.
-    */ 
-    public void herunterfahren() {
-        gpio.shutdown();
-        try {
-            for(int t = 0; t < this.inputPins.size(); t++) {
-                gpio.unprovisionPin(this.inputPins.get(t));
-            }
-        
-            for(int t = 0; t < this.outputPins.size(); t++) {
-                gpio.unprovisionPin(this.outputPins.get(t));
-            }
-        
-        } catch (java.lang.NullPointerException e) {
-            System.out.println("Error: Pin konnte nicht dereferenziert werden! Klasse: Tastenfeld (NullPointerException)");
-        }
     }
 }
